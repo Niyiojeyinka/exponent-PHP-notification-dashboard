dac082e0ec2c19198e541e9e4817f218967f0133
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-22 11:14:56 +0100

    removed branding

--- common/template.php
+++ common/template.php
@@ -5,20 +5,22 @@ function headerDisplay($title){
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title><?= $title?></title>
-<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>-->
-<link rel="Stylesheet" href="./assets/w3.css"/>
-<meta charset="utf-8">
+    <title><?= $title?></title>
+    <!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>-->
+    <link rel="Stylesheet" href="./assets/w3.css" />
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
-<body style="height:100vh;">
-<header class="w3-top w3-padding-large w3-pink">
-<span class="w3-medium w3-serif">Raja.NG's Admin Notification Dashboard</span>
 
-</header>
-<?php
+<body style="height:100vh;">
+    <header class="w3-top w3-padding-large w3-pink">
+        <span class="w3-medium w3-serif">Admin Notification Dashboard</span>
+
+    </header>
+    <?php
 
 
 }
@@ -27,35 +29,18 @@ function footerdisplay()
 {
     ?>
 
-<footer class="w3-padding-xxlarge w3-pink w3-center">
- &copy;2020
+    <footer class="w3-padding-xxlarge w3-pink w3-center">
+        &copy;2020
 
-</footer>
-<body>
-<html>
+    </footer>
 
-<?php
+    <body>
+        <html>
+
+        <?php
 
 }
 
 
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
